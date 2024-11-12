b3178e89e25ddb4081d471b4434df17a5e9e4078
Hendrik van Niekerk <hvanniek@redhat.com>
2024-11-12 12:08:28 +0000

    add missing line

--- prep-server.sh
+++ prep-server.sh
@@ -1,5 +1,7 @@
 #a small script to ensure
 python3.11 -m pip install --upgrade pip
 pip install -r requirements/requirements.txt
+pip install ansible-builder
+pip install ansible-navigator
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<< y
 ssh-copy-id ansible@192.168.1.50
