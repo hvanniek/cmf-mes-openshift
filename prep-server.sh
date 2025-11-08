fa7b3073b9af07c3c9894f80038132916ecdbc75
Hendrik van Niekerk <hvanniek@redhat.com>
2025-11-08 15:49:10 +0000

    update and add SMB configuration for shared data. Makes deployment compatible with SNO

--- prep-server.sh
+++ prep-server.sh
@@ -1,7 +1,8 @@
-#a small script to ensure
+#a small script to run on OpenShift devspaces if running on a blank CheCluster
+# sets up ansible and the required pieces to run playbooks
 python3.11 -m pip install --upgrade pip
 pip install -r requirements/requirements.txt
 pip install ansible-builder
 pip install ansible-navigator
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<< y
-ssh-copy-id ansible@192.168.1.50
+# ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<< y
+# ssh-copy-id ansible@192.168.1.50
